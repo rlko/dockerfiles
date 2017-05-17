58542054b958a36ec07701b9c0c71498735ec12d
lon <tw@eelon.moe>
2017-05-18 01:18:31 +0200

    Remove unecessary subdomain to domain

--- duckdns/update.sh
+++ duckdns/update.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ -z "$DUCK_SUBDOMAIN" ]; then
-    echo "DUCK_SUBDOMAIN not set"
+if [ -z "$DUCK_DOMAIN" ]; then
+    echo "DUCK_DOMAIN not set"
 fi
 
 if [ -z "$DUCK_TOKEN" ]; then
     echo "DUCK_TOKEN not set"
 fi
 
-if [ -n "$DUCK_SUBDOMAIN" ] && [ -n "$DUCK_TOKEN" ]; then
-    echo url="https://www.duckdns.org/update?domains=$DUCK_SUBDOMAIN.duckdns.org&token=$DUCK_TOKEN&ip=$DUCK_IP" | curl -k -K - -o /duck.log
+if [ -n "$DUCK_DOMAIN" ] && [ -n "$DUCK_TOKEN" ]; then
+    echo url="https://www.duckdns.org/update?domains=$DUCK_DOMAIN&token=$DUCK_TOKEN&ip=$DUCK_IP" | curl -k -K - -o /duck.log
 fi
