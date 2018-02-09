5b1944671abf8a59adbcb2546d1817042995a1c0
lon <tw@eelon.moe>
2018-02-09 13:05:52 +0100

    absolute path not needed now

--- rtorrent-flood-openvpn/scripts/connect_vpn.sh
+++ rtorrent-flood-openvpn/scripts/connect_vpn.sh
@@ -3,7 +3,7 @@
 set username "$env(OVPN_LOGIN)\r"
 set password "$env(OVPN_PASSWORD)\r"
 
-spawn openvpn --cd /config/ --config /config/$env(OVPN_CONFIG) --script-security 2
+spawn openvpn --cd /config/ --config $env(OVPN_CONFIG) --script-security 2
 expect "Enter Auth Username:"
 send $username
 expect "Enter Auth Password:"
